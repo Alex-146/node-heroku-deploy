d255575ccb162becc6fb9dc4021c5c9934dc4537
alex-146 <deynekos1488@gmail.com>
2021-05-03 15:03:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  const date = new Date();
-  const text = date.toISOString();
-  res.send(text);
+  const data = {
+    datetime: new Date().toISOString(),
+    node_env: app.get("NODE_ENV"),
+    port: app.get("PORT")
+  };
+  res.json(data);
 });
 
 const PORT = process.env.PORT ?? 5000;
 
 app.listen(PORT, () => {
   console.log(`Server has been started at ${PORT}...`);
+
+  app.set("NODE_ENV", process.env.NODE_ENV);
+  app.set("PORT", PORT);
 });
